54360a0487f8f52090e170ef6788ab2dcd6994d3
Grigorii Andrianov <27193731+gandrianov@users.noreply.github.com>
2026-03-04 16:14:50 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,9 @@ class CustomInstallCommand(install):
     def install_requirements(self):
         #subprocess.check_call(["conda", "install", "-c", "conda-forge", "rdkit", "xgboost", "pandas", "oddt", "-y"])
         #subprocess.check_call(["conda", "install", "-c", "openeye", "openeye-toolkits", "-y"])
-        subprocess.check_call(["pip", "install", "git+https://github.com/BioPandas/biopandas.git"])
-
+        # subprocess.check_call(["pip", "install", "git+https://github.com/BioPandas/biopandas.git"])
+        pass
+        
     def install_pyrosetta(self):
         #subprocess.check_call(["pip", "install", "pyrosetta-installer"])
         #subprocess.check_call(["python", "-c", "import pyrosetta_installer; pyrosetta_installer.install_pyrosetta()"])
